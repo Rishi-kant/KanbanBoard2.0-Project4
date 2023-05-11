5881be329e213fcac0bfb2bbd4713ae934a356eb
Rishi-Kant <kant3397@gmail.com>
2023-05-11 12:15:29 +0530

    working on nav

--- src/navBar/NavBar.jsx
+++ src/navBar/NavBar.jsx
@@ -3,8 +3,10 @@ import style from "./NavBar.module.css";
 function NavBar() {
   return (
     <div className={style.container}>
-      <div className={style.navContent}>
-        <h1>KanbanBoard</h1>
+      <div className={style.navContent}
+      contentEditable={true}
+      >
+        
       </div>
     </div>
   );
