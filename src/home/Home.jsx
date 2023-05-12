764e904bf39f275cab35b2bcd955cafaea94f842
codeManeesha <maneeshasingh19jun@gmail.com>
2023-05-12 10:41:14 +0530

    card editable component done

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,18 @@
 
+import Card from "../components/cards/Card"
 import style from "./Home.module.css"
 import React from 'react'
 
 function Home() {
   return (
+    <>
     <div className={style.container}> 
        <h1>This is home layout</h1>
+      
+     <Card/>
     </div>
+    
+    </>
   )
 }
 
