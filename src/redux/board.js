f731d04d5a34170a1c6f9a38d58d9d5bed673d7e
codeManeesha <maneeshasingh19jun@gmail.com>
2023-05-16 14:50:59 +0530

    editing card

--- src/redux/board.js
+++ src/redux/board.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import {v4 as uuid} from "uuid"
 const initialState = [];
 const boardSlice = createSlice({
   name: "board",
@@ -17,6 +17,7 @@ const boardSlice = createSlice({
       const { columnInd, task } = action.payload;
       state[columnInd].cards.push({
         task,
+        
         activity: [`Task created at the time : ${new Date()}`],
       });
     },
@@ -24,11 +25,21 @@ const boardSlice = createSlice({
       const {columnInd,taskIndex}=action.payload
       state[columnInd].cards.splice(taskIndex,1)
     },
+    editCard(state,action){
+      const { columnInd, taskIndex, cardName} = action.payload;
+    
+      state[columnInd].cards[taskIndex].task= cardName
+        
+        
+        
+      },
+        
+    
     delColumn(state,action){
      const{columnInd}=action.payload
      state.splice(columnInd,1)
     }
   },
 });
-export const { addColumn, addCard,delCard,delColumn } = boardSlice.actions;
+export const { addColumn, addCard,delCard,delColumn ,editCard} = boardSlice.actions;
 export default boardSlice.reducer;
