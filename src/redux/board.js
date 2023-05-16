555fb671413b8943704b21660078d4caa7e8c97b
Rishi-Kant <kant3397@gmail.com>
2023-05-17 00:55:43 +0530

    Added Content editable

--- src/redux/board.js
+++ src/redux/board.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {v4 as uuid} from "uuid"
+import { v4 as uuid } from "uuid";
 const initialState = [];
 const boardSlice = createSlice({
   name: "board",
@@ -9,7 +9,7 @@ const boardSlice = createSlice({
       const { title } = action.payload;
       state.push({
         title,
-        
+
         cards: [],
       });
     },
@@ -21,28 +21,34 @@ const boardSlice = createSlice({
         activity: [`Task created at the time : ${new Date()}`],
       });
     },
-    delCard(state,action){
-      const {columnInd,taskIndex}=action.payload
-      state[columnInd].cards.splice(taskIndex,1)
+    delCard(state, action) {
+      const { columnInd, taskIndex } = action.payload;
+      state[columnInd].cards.splice(taskIndex, 1);
     },
-    editCard(state,action){
-      const { columnInd, taskIndex, cardName} = action.payload;
-    
-      state[columnInd].cards[taskIndex].task= cardName
-        
-        
-        
-      },
-        
-    
-    delColumn(state,action){
-     const{columnInd}=action.payload
-     state.splice(columnInd,1)
-    }
- 
+    editCard(state, action) {
+      const { columnInd, taskIndex, cardName } = action.payload;
+
+      state[columnInd].cards[taskIndex].task = cardName;
+    },
+
+    delColumn(state, action) {
+      const { columnInd } = action.payload;
+      state.splice(columnInd, 1);
+    },
+    editColumnTitle(state, action) {
+      const { columnInd, newName } = action.payload;
+      state[columnInd].title = newName;
+    },
   },
 });
 
-export const { addColumn, addCard,delCard,delColumn ,editCard} = boardSlice.actions;
+export const {
+  addColumn,
+  addCard,
+  delCard,
+  delColumn,
+  editCard,
+  editColumnTitle,
+} = boardSlice.actions;
 
 export default boardSlice.reducer;
