4b4ef46eff3824f9ba6e577267b635c415df6481
Rishi-Kant <kant3397@gmail.com>
2023-05-19 14:57:59 +0530

    Removed unnecessary stuff

--- src/navBar/NavBar.jsx
+++ src/navBar/NavBar.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import style from "./NavBar.module.css";
-import { FiMoreHorizontal } from 'react-icons/fi';
-import {BsKanbanFill, BsStars } from 'react-icons/bs';
+
+import { BsKanbanFill, BsStars } from "react-icons/bs";
 import MoreBtn from "../components/moreButton/MoreBtn";
 
 function NavBar() {
   return (
     <div className={style.container}>
       <div className={style.innerContainer}>
-      
-      <div className={style.logo}> 
-      <BsKanbanFill className={style.icon}/> <h1>KanBan</h1>
-      </div>
-      <div className={style.back}>
-      
-        {/* <FiMoreHorizontal  className="more"/> */}
-        <MoreBtn />
-      </div>
+        <div className={style.logo}>
+          <BsKanbanFill className={style.icon} /> <h1>KanBan</h1>
+        </div>
+        <div className={style.back}>
+          <MoreBtn />
+        </div>
       </div>
     </div>
   );
